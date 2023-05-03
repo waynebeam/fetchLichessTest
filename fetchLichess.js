9adaf450113d36f91bd5305f1508786edf136053
waynebeam <waynejcbeam@me.com>
2023-05-03 09:45:21 -0400

    init
    First commit now that it is working. This sets some params and looks up the user. All the games are saved in a json games array. Each one can be accessed. This version just prints out the number of games and errors. Sometimes the JSON is corrupted. 20 games is the max.

---
+++ fetchLichess.js
@@ -0,0 +1,44 @@
+const params = {
+    max: 20,
+    rated: true,
+    pgnInJson: true,
+    perfType: 'blitz,rapid,classical'
+
+}
+
+const username = 'waynebeam';
+const url = `https://lichess.org/api/games/user/${username}?` + new URLSearchParams(params);
+
+
+async function getGames() {
+    const res = await fetch(url,
+        {
+            method: 'GET',
+            headers: { Accept: 'application/x-ndjson' },
+
+        }
+    );
+    if (res.statusText !== 'OK') {
+        console.log('busted');
+        return;
+    }
+
+    const data = await res.body;
+    const decoder = new TextDecoder();
+    let games = [];
+    let errors = 0;
+    for await (const chunk of data) {
+        const game = decoder.decode(chunk);
+        try {
+            games.push(JSON.parse(game));
+        }
+        catch (error) {
+            errors++;
+        }
+    }
+
+    console.log(`Got ${games.length} games with ${errors} errors`);
+
+}
+
+getGames();
